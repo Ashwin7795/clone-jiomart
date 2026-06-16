3fed1a11e318529e37045c289071981f5ea99a47
Ashwin <ashwinsreenath@gmail.com>
2026-06-16 13:05:08 +0530

    Implemented vendor product management APIs

--- backend/src/controllers/productController.js
+++ backend/src/controllers/productController.js
@@ -59,9 +59,91 @@ const getProductsBySubcategory = async (req, res) => {
   }
 };
 
+
+
+const createProduct = async (req, res) => {
+  try {
+    const product = await Product.create({
+      ...req.body,
+      vendorId: req.user.id,
+    });
+
+    res.status(201).json(product);
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+const updateProduct = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({
+        message: "Product not found",
+      });
+    }
+
+    if (product.vendorId.toString() !== req.user.id) {
+      return res.status(403).json({
+        message: "Not authorized",
+      });
+    }
+
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    res.status(200).json(updatedProduct);
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+const deleteProduct = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({
+        message: "Product not found",
+      });
+    }
+
+    if (product.vendorId.toString() !== req.user.id) {
+      return res.status(403).json({
+        message: "Not authorized",
+      });
+    }
+
+    await product.deleteOne();
+
+    res.status(200).json({
+      message: "Product deleted successfully",
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
 module.exports = {
   getProducts,
   getProductById,
   getProductsByCategory,
-  getProductsBySubcategory
+  getProductsBySubcategory,
+  createProduct,
+  updateProduct,
+  deleteProduct,
 };
+
--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -7,13 +7,43 @@ const {
   getProductById,
   getProductsByCategory,
   getProductsBySubcategory,
+  createProduct,
+  updateProduct,
+  deleteProduct,
 } = require("../controllers/productController");
 
+const {
+  protect,
+  vendorOnly,
+} = require("../middleware/authMiddleware");
+
 router.get("/", getProducts);
+
+router.post(
+  "/",
+  protect,
+  vendorOnly,
+  createProduct
+);
+
+router.put(
+  "/:id",
+  protect,
+  vendorOnly,
+  updateProduct
+);
+
+router.delete(
+  "/:id",
+  protect,
+  vendorOnly,
+  deleteProduct
+);
 
 router.get("/category/:category", getProductsByCategory);
 
 router.get("/subcategory/:subcategory", getProductsBySubcategory);
 
 router.get("/:id", getProductById);
+
 module.exports = router;
